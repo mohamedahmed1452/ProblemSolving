61317e43eded24536e6dfb3581c22ce9e240bb67
Mohamed Ahmed <20201700656@cis.asu.edu.ed>
2025-12-20 14:35:29 +0200

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Day01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           var x=TwoSum([2, 7, 11, 15],9);
+            foreach(var v in x)
+            {
+                Console.WriteLine(v);
+            }
+        }
+
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for(var i = 0; i < nums.Length; i++)
+            {
+                for(var j = 0; j < nums.Length; j++)
+                {
+                    if (i != j && nums[i] + nums[j] == target)
+                        return  [ i, j ];
+                }
+            }
+            return [];
+
+        }
+    }
+}
